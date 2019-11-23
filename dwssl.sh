e45561e97a622e056ca2b9878249e82b0d7250f6
Qiumi <7474@live.com>
2019-11-23 20:47:38 +0800

    no message

--- dwssl.sh
+++ dwssl.sh
@@ -2,7 +2,8 @@
 clear
 apt install curl -y
 echo 开始下载SSL证书
-rm -rf /root/ssl/991991
+rm -rf /root/ssl
+mkdir /root/ssl
 mkdir /root/ssl/991991
 curl https://raw.githubusercontent.com/addarmy/ssl/master/991991/fullchain.crt > /root/ssl/991991/fullchain.crt
 curl https://raw.githubusercontent.com/addarmy/ssl/master/991991/key.key > /root/ssl/991991/key.key
