a0a2498bdab9affa65e645a6cca9dacbcb926c87
Qiumi <7474@live.com>
2019-11-23 21:26:01 +0800

    no message

--- dwssl.sh
+++ dwssl.sh
@@ -4,6 +4,6 @@ echo 开始下载SSL证书
 rm -rf /root/ssl
 mkdir /root/ssl
 mkdir /root/ssl/991991
-curl https://raw.githubusercontent.com/addarmy/ssl/master/991991/fullchain.crt > /root/ssl/991991/fullchain.crt
+curl https://raw.githubusercontent.com/addarmy/ssl/master/991991/fullchain.cer > /root/ssl/991991/fullchain.cer
 curl https://raw.githubusercontent.com/addarmy/ssl/master/991991/key.key > /root/ssl/991991/key.key
 echo 证书下载完成
